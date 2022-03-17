e6f37d35f19169a109e870eede51228a415bde4b
Stacey Csikos <stacey@Staceys-MacBook-Pro.local>
2022-03-17 15:03:04 -0400

    finished with conditionals

--- script.js
+++ script.js
@@ -5,17 +5,27 @@ let two = document.querySelector(".two");
 // let towers = towerA + towerB
 // console.log(towerA.children.length)
 
-/// have to change so only top element is moveable. 
+/// have to change so only top element is moveable.
 /// maybe something to do with last drop? but then how
 /// will it affect the first move?
 if (towerA.children.length > 0) {
-  towerA.children[0].setAttribute("draggable", "true")
-  towerA.children[2].setAttribute("draggable", "false")
-};
+  // towerA.children[0].setAttribute("draggable", "true");
+  // towerA.children[2].setAttribute("draggable", "false");
+  // console.log(towerA.children);
+  let children = towerA.children;
+
+  for (let i = 0; i < children.length; i++) {
+    if (i === 0) {
+      children[i].setAttribute("draggable", "true");
+    } else {
+      children[i].setAttribute("draggable", "false");
+    }
+  }
+}
 
 function onDragStart(event) {
   event.dataTransfer.setData("text/plain", event.target.id);
-  console.log(towerA.children[0])
+  // console.log(towerA.children[0]);
 }
 
 function onDragOver(event) {
@@ -27,8 +37,8 @@ function onDragLeave(event) {
   event.target.classList.remove("drag-over");
 }
 
-function reset(){
-  left.append(...right.childNodes)
+function reset() {
+  left.append(...right.childNodes);
 }
 
 function onDrop(event) {
@@ -37,117 +47,94 @@ function onDrop(event) {
   const draggableElement = document.getElementById(id);
   const dropzone = event.target;
   let firstChild = dropzone.firstChild;
-  // lastdrop = dropzone.children[dropzone.children.length - 1]; // gets last item dropped
-  if (towerA && towerC.childElementCount === 4) {
-    let winner = document.querySelector(".winner")
-    winner.style.display = "block";
-    window.onclick = function () {
-      winner.style.display = "none"
-      setTimeout(reset, 2000)   // why didn't function work inside of here. 
+  function checkWinner() {
+    if (towerA.children.length === 0 && towerC.children.length === 5) {
+      let winner = document.querySelector(".winner");
+      winner.style.display = "block";
+      window.onclick = function () {
+        winner.style.display = "none";
+        setTimeout(reset, 2000);
+      };
     }
   }
 
-  if (dropzone.className === "place-holder") {  
-    if (firstChild.firstChild) {
-      let x = parseInt(draggableElement.id)
-      let y = parseInt(firstChild.id)
+  if (dropzone.className === "place-holder") {
+    if (firstChild) {
+      console.log("Part 1");
+      let x = parseInt(draggableElement.id);
+      let y = parseInt(firstChild.id);
+      let z = parseInt(firstChild?.nextElementSibling?.id) || false;
+      console.log(firstChild);
+      console.log(`x: ${x}`, `y: ${y}`, `z: ${z}`);
       if (x < y) {
-      dropzone.insertBefore(draggableElement, firstChild); 
+        console.log("Part 2");
+        dropzone.insertBefore(draggableElement, firstChild);
         event.dataTransfer.clearData();
+        checkWinner();
+      } else if (z) {
+        console.log("Part 3");
+        if (!y && x < z) {
+          console.log("Part 4");
+          dropzone.insertBefore(
+            draggableElement,
+            firstChild.nextElementSibling
+          );
+          event.dataTransfer.clearData();
+        } else {
+          console.log("Part 5");
+          event.dataTransfer.clearData();
+        }
       } else {
-        event.dataTransfer.clearData();
+        if (!y && !z) {
+          dropzone.insertBefore(
+            draggableElement,
+            firstChild.nextElementSibling
+          );
+          event.dataTransfer.clearData();
+        } else {
+          console.log("Part 6");
+          event.dataTransfer.clearData();
+        }
       }
     } else {
-        dropzone.insertBefore(draggableElement, firstChild); 
-        event.dataTransfer.clearData();
-    }
-    // towerA.children[0].setAttribute("draggable", "true")
-
-    if (towerA.children.length >= 0) {
-      towerA.children[0].setAttribute("draggable", "true")
-      towerA.children[1].setAttribute("draggable", "false")
-    }
-
-    if (towerB.children.length >= 0) {
-      towerB.children[0].setAttribute("draggable", "true")
-      towerB.children[1].setAttribute("draggable", "false")
-    };
-    if (towerC.children.length >= 0) {
-      towerC.children[0].setAttribute("draggable", "true")
-      towerC.children[1].setAttribute("draggable", "false")
+      console.log("Part 7");
+      dropzone.insertBefore(draggableElement, firstChild);
+      event.dataTransfer.clearData();
     }
   }
 
-  
+  if (towerA.children.length >= 0) {
+    let towerAchildren = towerA.children;
 
-  
+    for (let i = 0; i < towerAchildren.length; i++) {
+      if (i === 0) {
+        towerAchildren[i].setAttribute("draggable", "true");
+      } else {
+        towerAchildren[i].setAttribute("draggable", "false");
+      }
+    }
+  }
 
+  if (towerB.children.length >= 0) {
+    let towerBchildren = towerB.children;
+
+    for (let i = 0; i < towerBchildren.length; i++) {
+      if (i === 0) {
+        towerBchildren[i].setAttribute("draggable", "true");
+      } else {
+        towerBchildren[i].setAttribute("draggable", "false");
+      }
+    }
+  }
+  if (towerC.children.length >= 0) {
+    let towerCchildren = towerC.children;
+
+    for (let i = 0; i < towerCchildren.length; i++) {
+      if (i === 0) {
+        towerCchildren[i].setAttribute("draggable", "true");
+      } else {
+        towerCchildren[i].setAttribute("draggable", "false");
+      }
+    }
+  }
 }
-
-
-//need to make it so the dropable is only the divs. 
-
-
-// console.log(id)
-// dropzone.insertBefore (draggableElement, dropzone.lastElementChild)
-// console.log(draggableElement) dropzone.children[0].id
-// console.log(dropzone.children)
-
-// dropzone.appendChild(draggableElement);
-// dropzone.insertBefore(draggableElement, dropzone.firstChild)
-
-// if (dropzone.childNodes.length > 0) {
-//   x = parseInt(dropzone.lastElementChild.id)
-//   y = parseInt(dropzone.children[1].id)
-//   console.log (x, y, x+y)
-// }
-
-// let three = document.querySelector(".three");
-// let placeHolders = document.querySelectorAll(".place-holder");
-
-// three.addEventListener("dragstart", dragStart);
-
-// function dragStart(event) {
-//   event.dataTransfer.setData("text", e.target.id);
-
-// }
-
-// // placeHolders.forEach((placeHolder) => {
-// //   placeHolder.addEventListener("dragenter", dragEnter);
-// //   placeHolder.addEventListener("dragover", dragOver);
-// //   placeHolder.addEventListener("dragleave", dragLeave);
-// //   placeHolder.addEventListener("drop", drop);
-// // });
-
-// function dragEnter(event) {
-//   event.preventDefault();
-//   event.target.classList.add("drag-over");
-// }
-
-// function dragOver(event) {
-//   event.preventDefault();
-//   event.target.classList.add("drag-over");
-// }
-
-// function dragLeave(event) {
-//   event.target.classList.remove("drag-over");
-// }
-
-// function drop(event) {
-//   console.log(event)
-//   event.preventDefault()
-//   event.target.classList.remove("drag-over");
-//   const id = event.dataTransfer.getData("text")
-//   const draggableElement = document.getElementById(id)
-//   const dropZone = event.target
-//   console.log(dropZone)
-//   dropZone.appendChild(draggableElement)
-//   event.dataTransfer.clearData()
-
-//   // const id = e.dataTransfer.getData("text/plain");
-//   // console.log(id)
-//   // const draggable = document.getElementById(id);
-//   // console.log(draggable)
-//   // e.target.appendChild(draggable);
-//   // draggable.classList.remove("hide");
-// }
